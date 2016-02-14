69a6d089b53cbecd265c887fb1c27fd3850e33a6
Daniel Andefors <daniel.andefors@gmail.com>
2016-02-13 18:52:47 -0800

    Javadoc (mostly).

--- src/main/java/dandefors/miscellanea/Random.java
+++ src/main/java/dandefors/miscellanea/Random.java
@@ -12,10 +12,28 @@ public class Random {
 
     private long seed;
 
+    /**
+     * Create a new random number generator.
+     */
+    public Random() {
+        this(System.nanoTime());
+    }
+
+    /**
+     * Create a new random number generator with the given seed value.
+     *
+     * @param seed The seed.
+     */
     public Random(long seed) {
         this.seed = seed;
     }
 
+    /**
+     * Generate a pseudo random number with the given number of bits (1-32).
+     *
+     * @param bits The number of bits.
+     * @return The next random number, truncated to the given number of bits.
+     */
     public int next(int bits) {
         seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
         return (int) (seed >>> (48 - bits));
